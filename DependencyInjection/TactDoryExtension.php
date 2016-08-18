3afcf5821174b3247bb84621bb17f9ed1e03c6be
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2016-08-18 17:45:00 +0200

    Close #8795

--- DependencyInjection/TactDoryExtension.php
+++ DependencyInjection/TactDoryExtension.php
@@ -40,10 +40,10 @@ class TactDoryExtension extends Extension implements PrependExtensionInterface
      *
      */
     public function load(array $configs, ContainerBuilder $container) {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
+//         $configuration = new Configuration();
+//         $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+//         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+//         $loader->load('services.yml');
     }
 }
