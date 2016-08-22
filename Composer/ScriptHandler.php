dee0cbf75b1b38c54e4501c01145e0d91c9f54da
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2016-08-22 11:57:34 +0200

    Update #8881 add some stuffs

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -42,8 +42,23 @@ class ScriptHandler
              "resource: \"@TactDoryBundle/Resources/config/routing.yml\"\r\n" . self::TAB . "prefix: /\r\n";
 
     /**
+     * The parameters to add at dist.
+     *
+     * @var array
      */
-    public static function tempTest() {
+    private static $requiredParameters = [
+        'database_driver' => 'pdo_mysql',
+        'database_type' => 'mysql',
+        'router.request_context.host' => 'example.com',
+        'router.request_context.scheme' => 'http',
+        'router.request_context.base_url' => ''
+    ];
+
+    /*
+     * Method used only for tests.
+     */
+    private static function tempTest()
+    {
         $content = '# This file is auto-generated during the dory install' . self::ENDL . 'parameters:' . self::ENDL .
                  self::TAB . 'database_driver: pdo_mysql' . self::ENDL . self::TAB . 'database_type: mysql' . self::ENDL .
                  self::TAB . 'database_host: 127.0.0.1' . self::ENDL . self::TAB . 'database_port: 3306' . self::ENDL .
@@ -53,25 +68,28 @@ class ScriptHandler
                  self::TAB . 'mailer_password: null' . self::ENDL . self::TAB . 'locale: en' . self::ENDL . self::TAB .
                  'secret: ThisTokenIsNotSoSecretChangeIt';
 
-        file_put_contents(sprintf('%s/%s', self::PROJECT_ROOT_PATH, 'app/config/parameters.yml'), $content);
+        // file_put_contents(sprintf('%s/%s', self::PROJECT_ROOT_PATH, 'app/config/parameters.yml'), $content);
     }
 
     /**
      * Install the dory bundle.
      */
-    public static function install() {
+    public static function install()
+    {
         echo "Install TACT Dory Bundle";
 
         self::copyScripts();
         self::addRouting();
         self::updateAppKernel();
         self::updateConfig();
+        self::updateParameterDist();
     }
 
     /**
      * Generate the ant file.
      */
-    private static function copyScripts() {
+    private static function copyScripts()
+    {
         foreach (scandir(self::SCRIPT_DIRECTORY) as $script) {
             $source = sprintf('%s/%s', self::SCRIPT_DIRECTORY, $script);
             $destination = sprintf('%s/%s', self::PROJECT_ROOT_PATH, $script);
@@ -85,7 +103,8 @@ class ScriptHandler
     /**
      * Add the routing dependencies.
      */
-    private static function addRouting() {
+    private static function addRouting()
+    {
         if (strpos(file_get_contents(self::ROUTING_DESTINATION_PATH), 'TactDoryBundle') == false) {
             file_put_contents(self::ROUTING_DESTINATION_PATH, self::ROUTING_CONTENT, FILE_APPEND);
         }
@@ -94,7 +113,8 @@ class ScriptHandler
     /**
      * Add our bundles into AppKernel.
      */
-    private static function updateAppKernel() {
+    private static function updateAppKernel()
+    {
         $filepath = self::APP_KERNEL_DESTINATION_PATH;
         $content = file_get_contents($filepath);
 
@@ -110,18 +130,41 @@ class ScriptHandler
      */
     private static function updateConfig()
     {
-        $filepath = sprintf('%s/%s', self::PROJECT_ROOT_PATH,'app/config/config.yml');
+        $filepath = sprintf('%s/%s', self::PROJECT_ROOT_PATH, 'app/config/config.yml');
         $content = file_get_contents($filepath);
 
         if (strpos($content, 'DoryBundle') == false) {
-           $newValue= 'imports:'.self::ENDL.self::TAB.'- { resource: "@TactDoryBundle/Resources/config/config.yml" } # Not loaded by Dory extention due to needed execution order.';
+            $newValue = 'imports:' . self::ENDL . self::TAB .
+                     '- { resource: "@TactDoryBundle/Resources/config/config.yml" } # Not loaded by Dory extention due to needed execution order.';
 
             if (strpos($content, 'imports:')) {
                 $content = str_replace('imports:', $newValue, $content);
-            } else{
-                $content = $newValue.self::ENDL.$content;
+            } else {
+                $content = $newValue . self::ENDL . $content;
             }
 
+            file_put_contents($filepath, $content);
+        }
+    }
+
+    /**
+     * Update parameters.dist file to add the necessary parameters.
+     */
+    private static function updateParameterDist()
+    {
+        $modified = false;
+        $filepath = sprintf('', self::PROJECT_ROOT_PATH, 'app/config/parameters.yml.dist');
+        $content = file_get_contents($filepath);
+
+        foreach (self::$requiredParameters as $parameter => $defaultValue) {
+            if (strpos($content, $parameter)) {
+                $newParameter = sprintf('    %s: %s', $parameter, $defaultValue);
+
+                $modified = true;
+            }
+        }
+
+        if ($modified === true) {
             file_put_contents($filepath, $content);
         }
     }
